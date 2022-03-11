6f3373d69eee23d1a5e76a291cd8d5fa58d21479
sbodur <sbodur@mail.com>
2022-03-11 15:14:35 +0300

    Initial commit

---
+++ src/main/java/com/example/invoiceaccounting/InvoiceAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.example.invoiceaccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceAccountingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceAccountingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/invoiceaccounting/InvoiceAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.invoiceaccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceAccountingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
